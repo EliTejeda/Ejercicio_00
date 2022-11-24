3f4e2189fc49a7f6690cf7adb00463e800e77d52
Eli Tejeda <tejeda.eliza@gmail.com>
2022-11-24 15:03:02 -0600

    first commit

---
+++ home-vista.js
@@ -0,0 +1,16 @@
+import { LitElement, html, css } from 'lit';
+
+export class HomeVista extends LitElement {
+    static styles = [
+        css`
+            :host {
+                display: block;
+            }
+        `
+    ];
+
+    render() {
+        return html``;
+    }
+}
+customElements.define('home-vista', HomeVista);
